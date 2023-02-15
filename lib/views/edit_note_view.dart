dddd237c559c8156c33e134ce3acbbb5c17dbf45
ahmed metwally <ahmed.mmetwally50@gmail.com>
2023-02-15 06:20:22 +0200

    add note view body

---
+++ lib/views/edit_note_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app_bloc_local_database/views/widgets/edit_note_view_body.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const EditNoteViewBody();
+  }
+}
--- lib/views/widgets/custome_note_item.dart
+++ lib/views/widgets/custome_note_item.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app_bloc_local_database/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(margin: EdgeInsets.symmetric(vertical: 5),decoration: BoxDecoration(borderRadius: BorderRadius.circular(16),color: Color(0xffFFCD79)),
-      child: Padding(
-        padding: const EdgeInsets.only(top:20,left: 10,bottom: 20),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.end,children: [
-          ListTile(title: Text("Flutter tipcs",style: const TextStyle(fontSize: 26,color: Colors.black),),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child:Text("Build your career with ahmed metwally",style: TextStyle(fontSize: 18,color: Colors.black.withOpacity(.4)),),
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>const EditNoteView()));
+      },
+      child: Container(margin: EdgeInsets.symmetric(vertical: 5),decoration: BoxDecoration(borderRadius: BorderRadius.circular(16),color: Color(0xffFFCD79)),
+        child: Padding(
+          padding: const EdgeInsets.only(top:20,left: 10,bottom: 20),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.end,children: [
+            ListTile(title: Text("Flutter tipcs",style: const TextStyle(fontSize: 26,color: Colors.black),),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child:Text("Build your career with ahmed metwally",style: TextStyle(fontSize: 18,color: Colors.black.withOpacity(.4)),),
+              ),
+              trailing: const Icon(FontAwesomeIcons.trash,color: Colors.black,size:30 ,),
             ),
-            trailing: const Icon(FontAwesomeIcons.trash,color: Colors.black,size:30 ,),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: Text("May 21,2023",style: TextStyle(fontSize: 18,color: Colors.black.withOpacity(.4))),
-          ),
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Text("May 21,2023",style: TextStyle(fontSize: 18,color: Colors.black.withOpacity(.4))),
+            ),
 
-        ],),
-      ),);
+          ],),
+        ),),
+    );
   }
 }
